11d6a43173e376aa02fdfcb8d2bdfbfd9358c53d
sn037 <y-murakami@use-eng.co.jp>
2019-06-19 20:23:56 +0900

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './/app-routing.module';
 import { NotFoundComponent } from './error/not-found/not-found.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
+import { HogehogeComponent } from './hogehoge/hogehoge.component';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { LoginComponent } from './login/login.component';
     AppComponent,
     NotFoundComponent,
     SignUpComponent,
-    LoginComponent
+    LoginComponent,
+    HogehogeComponent
   ],
   imports: [
     BrowserModule,
