85c25c7ab7227db7c2ecac605e3218a2fd5a6d05
yukimamura <sn037use@gmail.com>
2019-06-23 23:38:33 +0900

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './error/not-found/not-found.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
-import { AuthGuard } from './auth.guard';
+//import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: './timeline/timeline.module#TimelineModule' },
